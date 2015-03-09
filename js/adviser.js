750ba87264515136ad058a2bcb0b6c856d8db241
dainiannian <dai_niannian@189.cn>
2015-03-09 10:39:42 +0800

    2015.3.9
    2015.3.9

--- js/adviser.js
+++ js/adviser.js
@@ -1,28 +1,5 @@
 
 $(document).ready(function() {
-    //修改宽度样式开始
-	$('.contact_item li').each(function (index){
-	 	var iHited = (index+1)%4 ; 
-	 	if (0 == iHited) {
-	 		$('.contact_item li').eq(index).css({'margin-right':'0px'});	
-	 	};	
- 	});
-	//修改宽度样式结束
-  
-  //照片遮罩
-  $('.person_picture').each(function (index){
-      $(this).hover(function(){
-          $(this).find('.mask').addClass('show');
-          $(this).find('.triangle').addClass('appear');
-
-      },function(){
-          $(this).find('.mask').removeClass('show');
-          $(this).find('.triangle').removeClass('appear');
-      });
-  });
-
-
-
   //上一页，下一页样式
     $('.b_page').each(function (index){
        $(this).click(function(){
